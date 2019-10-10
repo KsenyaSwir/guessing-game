98b75ffd86052c0ae5d8f43bd35b0e662a335556
Kseniya Sviridova <ksyu.sviridova.eger@mail.ru>
2019-10-10 14:42:08 +0300

    feat: add task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,27 @@
 class GuessingGame {
-    constructor() {}
+    constructor(min, max, middle) {
+        this.min = null;
+        this.max = null;
+        this.middle = null;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
+        this.middle = Math.round(this.min + (this.max - this.min) / 2);
 
+        return this.middle;
     }
 
     lower() {
-
+        this.max = this.middle;
     }
 
     greater() {
-
+        this.min = this.middle;
     }
 }
 
